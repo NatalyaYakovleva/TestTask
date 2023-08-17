180c21fe2cb0e91c9bfac12a3eb8ebe19d69bc38
Natalya <ipromaster@inbox.ru>
2023-08-17 08:37:13 +0300

    adding news block and editing code

--- bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
+++ bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001691756881';
-$dateexpire = '001691760541';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1691756882;}';
+$datecreate = '001692250261';
+$dateexpire = '001692253921';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1692250262;}';
 return true;
 ?>
--- bitrix/managed_cache/MYSQL/b_option/e6/e69b462580b3cd8550c28eb99ac27172.php
+++ bitrix/managed_cache/MYSQL/b_option/e6/e69b462580b3cd8550c28eb99ac27172.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001691754964';
-$dateexpire = '001691758564';
+$datecreate = '001692250021';
+$dateexpire = '001692253621';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:2:{s:20:"ipcheck_disable_file";s:64:"/bitrix/modules/ipcheck_disable_8178050944a73d9c3a288c7a117605f5";s:12:"redirect_sid";s:32:"2l4wqd34qi4ai65y4ueqckdltmoaz1fb";}}}';
 return true;
 ?>
